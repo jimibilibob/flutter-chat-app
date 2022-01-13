6dcbd2bec592c66e5f92a75460043f12d8fd4524
Israel Aguilar <isra.neri2@gmail.com>
2022-01-12 23:00:28 -0400

    added pull to refresh into user screen

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -2,7 +2,7 @@ import 'package:chat/screens/screens.dart';
 import 'package:flutter/material.dart';
 
 final Map<String, Widget Function(BuildContext)> appRoutes = {
-  'users': (_) => const UsersScreen(),
+  'users': (_) => UsersScreen(),
   'hat': (_) => const ChatScreen(),
   'register': (_) => const RegisterScreen(),
   'login': (_) => const LoginScreen(),
--- lib/screens/users_screen.dart
+++ lib/screens/users_screen.dart
@@ -1,14 +1,100 @@
+import 'package:chat/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:pull_to_refresh/pull_to_refresh.dart';
 
-class UsersScreen extends StatelessWidget {
+class UsersScreen extends StatefulWidget {
   const UsersScreen({Key? key}) : super(key: key);
 
   @override
+  State<UsersScreen> createState() => _UsersScreenState();
+}
+
+class _UsersScreenState extends State<UsersScreen> {
+  final _refreshController = RefreshController(initialRefresh: false);
+
+  final users = [
+    User(online: true, email: 'test1@test.com', name: 'Maria', uid: '1'),
+    User(online: false, email: 'test2@test.com', name: 'José', uid: '2'),
+    User(online: true, email: 'test1@test.com', name: 'Marco', uid: '3')
+  ];
+
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'My name',
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ),
+        elevation: 1,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.exit_to_app,
+            color: Colors.black87,
+          ),
+        ),
+        actions: [
+          Container(
+            margin: const EdgeInsets.only(right: 10),
+            child: const Icon(
+              Icons.offline_bolt,
+              color: Colors.red,
+            ),
+          )
+        ],
+      ),
+      body: SmartRefresher(
+        controller: _refreshController,
+        child: _usersListView(),
+        enablePullDown: true,
+        onRefresh: _loadUsers(),
+        header: WaterDropHeader(
+          complete: Icon(
+            Icons.check,
+            color: Colors.blue[400],
+          ),
+          waterDropColor: Colors.blue[400]!,
+        ),
       ),
     );
+  }
+
+  ListView _usersListView() {
+    return ListView.separated(
+      physics: const BouncingScrollPhysics(),
+      itemBuilder: (_, i) => _userListTile(users[i]),
+      separatorBuilder: (_, i) => const Divider(),
+      itemCount: users.length,
+    );
+  }
+
+  ListTile _userListTile(User user) {
+    return ListTile(
+      title: Text(user.name),
+      subtitle: Text(user.email),
+      leading: CircleAvatar(
+        child: Text(user.name.substring(0, 2)),
+        backgroundColor: Colors.blue[100],
+      ),
+      trailing: Container(
+        width: 10,
+        height: 10,
+        decoration: BoxDecoration(
+          color: user.online ? Colors.green[300] : Colors.red,
+          borderRadius: BorderRadius.circular(100),
+        ),
+      ),
+    );
+  }
+
+  _loadUsers() async {
+    // monitor network fetch
+    await Future.delayed(const Duration(milliseconds: 1000));
+    // if failed,use refreshFailed()
+    _refreshController.refreshCompleted();
   }
 }
