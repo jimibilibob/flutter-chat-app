338d85f40541e23d5d949e9f3377ab90e72f2dd3
Israel Aguilar <isra.neri2@gmail.com>
2022-01-14 23:50:45 -0400

    Chat screen design

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -5,9 +5,49 @@ class ChatScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hola Mundo'),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Column(
+          children: [
+            CircleAvatar(
+              child: Text(
+                'Te',
+                style:
+                    TextStyle(fontSize: 12, backgroundColor: Colors.blue[100]),
+              ),
+              maxRadius: 14,
+            ),
+            const SizedBox(
+              height: 3,
+            ),
+            const Text(
+              'Melissa Flores',
+              style: TextStyle(color: Colors.black87, fontSize: 12),
+            )
+          ],
+        ),
+        centerTitle: true,
+        elevation: 1,
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Flexible(
+                child: ListView.builder(
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: (_, i) => Text('$i'),
+              reverse: true,
+            )),
+            const Divider(
+              height: 1,
+            ),
+            Container(
+              color: Colors.white,
+              height: 50,
+            )
+          ],
+        ),
       ),
     );
   }
